eb16143a1da330f252ceaa21ef0c6b72d6a3ae93
kohei-bit <akusadss@gmail.com>
2021-04-02 15:14:16 +0900

    コミットのテスト

---
+++ src/intro/Variable2.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class Variable2 {
+    public static void main(String[] args) {
+
+        int i = 500;
+        char c = '4';
+        double d = 7.77;
+
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
