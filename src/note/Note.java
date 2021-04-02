4b09c6907fa1c4b6deda086812d289f3c7e58c3a
kohei-bit <akusadss@gmail.com>
2021-04-02 15:28:26 +0900

    コミットのテスト

---
+++ src/note/Note.java
@@ -0,0 +1,7 @@
+package note;
+
+public class Note {
+    public static void main(String[] args) {
+        System.out.println("今日学んだこと新規パッケージは小文字　新規クラスは小文字　mainメソッドは器　soutで記述　ダブルクォーテーション");
+    }
+}
