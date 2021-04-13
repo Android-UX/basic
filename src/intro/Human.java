037c16e4d27932f9d4d7371a32bddd06a7b1136c
kohei-bit <akusadss@gmail.com>
2021-04-13 15:19:27 +0900

    コミット

--- src/intro/Human.java
+++ src/intro/Human.java
@@ -1,22 +1,22 @@
 package intro;
 
-//人間クラス　humanクラスのひな型
-public class Human {
-    //フィールド
+//人間クラス　humanクラスのひな型　器
+    class Human {
+    //フィールド データを記憶する場所　情報を保存
     private String name;//文字列の名前 private=自分の中でしか見られない情報　外からは見れない カプセル化　書き換えられないように隠ぺいする
     private String gender;//文字列の性別
     private int age;//整数の年齢
 
-    //コンストラクタ　インスタンスを作る時に使用されるメソッド
-    public Human(String name, String gender, int age) {
-        this.name = name;
+    //コンストラクタ　インスタンスを作る時に使用される専用のメソッド　機能　名前、性別、年齢を外からもらって答える　情報を外部から受け取って実体を作る
+    public Human(String name, String gender, int age) {//必ずpublic クラス名()引数
+        this.name = name;//これの情報　自分自身の情報 this = それぞれのインスタンス毎の
         this.gender = gender;
         this.age = age;
     }
 
     //ゲッター　データを取得するメソッド
-    public String getName() {
-        return name;
+    public String getName() {//publicはどこからでも見れるアクセス修飾子　String戻り値の型 getName()メソッド名
+        return name;//名前を返す　答えを返却する　値を返す　関数とかファンクション
     }
 
     public String getGender() {
@@ -27,4 +27,12 @@ public class Human {
         return age;
     }
 
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' + //エスケープシーケンス
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/intro/HumanSample.java
+++ src/intro/HumanSample.java
@@ -3,12 +3,22 @@ package intro;
 //人間クラスのインスタンスを作成する　プログラムに必要な情報をまとめたもの
 public class HumanSample {
     public static void main(String[] args) {
-        //インスタンスを作成する
-        Human human1 = new Human("やまだ","男",35);
+        //インスタンスを作成する 単体で使うのではなく他のプログラムから呼び出す
+        Human human1 = new Human("やまだ","男",35);//別の器
+        Human human2 = new Human("たけだ","女",24);//別の器
+
 
         //各データを表示する
-        System.out.println(human1.getName());
+        System.out.println(human1.getName());//メソッドを呼び出している あなたの名前は何ですか？get+フィールド名 get大文字メソッド（キャメル）
         System.out.println(human1.getGender());
         System.out.println(human1.getAge());
+
+        System.out.println(human2.getName());
+        System.out.println(human2.getGender());
+        System.out.println(human2.getAge());
+
+        System.out.println(human1);
+        System.out.println(human2);
     }
+
 }
