3d5321664ef05282d9c871dac3d549b257146001
kohei-bit <akusadss@gmail.com>
2021-05-11 14:46:30 +0900

    5/11

--- src/objectSample/enumSample/Color.java
+++ src/objectSample/enumSample/Color.java
@@ -3,5 +3,20 @@ package objectSample.enumSample;
 
 //enumのサンプル 定数を定義しているクラス
 enum Color {
-    RED, BLUE, YELLOW;
+    RED("赤"),
+    BLUE("青"),
+    YELLOW("黄");
+
+    //フィールド
+    private String jpName;
+
+    //コンストラクタ
+    private Color(String jpName) {
+        this.jpName = jpName;
+    }
+
+    //ゲッター
+    public String getJpName() {
+        return jpName;
+    }
 }
--- src/objectSample/enumSample/EnumSample.java
+++ src/objectSample/enumSample/EnumSample.java
@@ -17,7 +17,16 @@ class EnumSample {
     }
     public static void main(String[] args) {
         //列挙型を表示してみる　引数とかコンストラクタの引数にある
-        System.out.println(Color.RED);
+        System.out.println(Color.RED);//RED
         viewColor(Color.YELLOW);//受け渡すデータに制約を課すことができる　3種類だけ　絶対に正しい値しか入れられないようにする　他を省く
+        System.out.println(Color.BLUE.getJpName());//赤
+        System.out.println(Color.RED.name());//RED
+        System.out.println(Color.BLUE.ordinal());//1
+        Color color = Color.valueOf("YELLOW");//列挙型の取得
+        System.out.println(color.getJpName());//黄
+        Color[] colors = color.values();//列挙定数の一覧の取得
+        for (Color c:colors){
+            System.out.println(c);
+        }
     }
 }
