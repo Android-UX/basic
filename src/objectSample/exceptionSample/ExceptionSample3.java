a1da58d809f12ee864d0b62a0b97df3550949d55
kohei-bit <akusadss@gmail.com>
2021-05-12 14:14:01 +0900

    5/12

---
+++ src/objectSample/exceptionSample/ExceptionSample3.java
@@ -0,0 +1,22 @@
+package objectSample.exceptionSample;
+
+//例外をスローするメソッドの書き方
+class ExceptionSample3 {
+    public static int getLength(String word) throws Exception{
+        try {
+            return  word.length();
+        }catch (Exception e) {
+            throw new Exception("キーワードが異常です");//報告している　exceptionクラスのコンストラクタ
+        }
+    }
+    public static void main(String[] args) {
+        //メソッドで検査例外をスローするとtry catchブロックを書く必要がある
+        try {
+            System.out.println(getLength("abc"));
+            System.out.println(getLength(null));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());//例外メッセージの表示
+        }
+    }
+}
